816be2c6f8d18baed5628005519d26019efeb277
skimaskdave <davtg123@gmail.com>
2022-12-12 17:19:56 +0000

    Day 5 Part 1 & 2
    This took me way too much time but it is done. Never again.

---
+++ Day5/part1.py
@@ -0,0 +1,42 @@
+# David Grasmeder AoC 2022 solution to day 5 part 1
+import re
+
+f = open("input.txt", "rt")
+toggle = False
+moves = []
+stack_lines = []
+for line in f:
+    if line == "\n":
+        toggle = True
+    if not toggle:
+        stack_lines.append(line)
+    else:
+        if line.strip() != "":
+            moves.append(line.strip())
+
+stacks = [[], [], [], [], [], [], [], [], []]
+for i in range(len(stack_lines) - 1):       # get stacks sorted
+    temp = [stack_lines[i][x:x+4] for x in range(0, len(stack_lines[i]), 4)]
+    for j in range(len(temp)):
+        if temp[j].strip() != "":
+            stacks[j].append(temp[j].strip())
+
+regex = "\d"
+instructions = []
+for instr in moves:     # gets instructions for crates
+    temp = []
+    dets = instr.split(" ")
+    for d in dets:
+        if re.search(regex, d):
+            temp.append(int(d))
+    instructions.append(temp)
+
+for instr in instructions:
+    for i in range(instr[0]):
+        stacks[instr[2]-1].insert(0, stacks[instr[1]-1].pop(0))
+
+for s in stacks:
+    print(s)
+
+for s in stacks:
+    print(s.pop(0), end="")
